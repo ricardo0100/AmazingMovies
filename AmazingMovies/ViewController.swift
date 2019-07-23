49715edb5306b4f4ce314ee24a5839e6572dd3ce
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2019-07-23 18:11:07 -0300

    Create new Xcode project

---
+++ AmazingMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmazingMovies
+//
+//  Created by Ricardo Gehrke Filho on 23/07/19.
+//  Copyright © 2019 Ricardo Gehrke Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
